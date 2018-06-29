095297df9364a352e1afcc20fe9480988ca4f2f5
Clifton Craig <clifton.craig@gmail.com>
2018-06-28 19:28:00 -0700

    Adds logic to read keys from env variables.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 8080;
 
 let theKeys = {
-    firebase:'your Firebase key here',
-    google:'your Google key here'
+    firebase: process.env.FIREBASE,
+    google: process.env.GOOGLE
 };
 
 app.get('/keys', (req, res) => res.send(JSON.stringify(theKeys)));
