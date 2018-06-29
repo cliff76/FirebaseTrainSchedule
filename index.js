30b82d49f154f07376df8ab04c446fad9e9b00cf
Clifton Craig <clifton.craig@gmail.com>
2018-06-28 19:07:51 -0700

    Adds path to the keys on the server.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+let theKeys = {
+    firebase:'your Firebase key here',
+    google:'your Google key here'
+};
+
+app.get('/keys', (req, res) => res.send(JSON.stringify(theKeys)));
 app.use(express.static('www'));
 
 app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
